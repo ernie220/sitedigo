980efd8e65dd39c14b835380e15474c4d8705cfd
Ernesto Nunez <ernie220@gmail.com>
2013-01-15 12:26:21 -0500

    Do this again

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sitedigo::Application.config.secret_token = '2daaaa402cc4731c563655daffa8aac03ff6fd821bf33313d74007cd9816830bd8585c948ff0ecc4409ba29f5dc8ce47333f27bf84b788faa4be6e027c1ccd19'
